58d39c3aa7b2d367e092c74af74a110d52b9ffd1
月佬 <johnpanda13@gmail.com>
2024-10-21 17:46:28 +0800

    Update main.py, able to download multiple video at once

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import yt_dlp
 from dataclasses import dataclass
 from pathlib import Path
 from tkinter import *
+from tkinter import filedialog
 from tkinter.ttk import Button, Entry
+from PIL import Image, ImageTk
+import os
 
 @dataclass
 class VideoInfo:
@@ -43,17 +46,17 @@ class VideoDownloader:
                 )
         except Exception as e:
             print(f"Error getting video info: {e}")
-            return None
+            return f"Error getting video info: {e}"
 
     def download_video(self, video_info:VideoInfo) -> bool:
         try:
             print(f"Downloading: {video_info.title}")
             with yt_dlp.YoutubeDL(self.ydl_opts) as v:
                 v.download([video_info.url])
-            return True
+            return "Download completed successfully."
         except Exception as e:
             print(f"Download failed: {e}")
-            return False
+            return f"Download failed: {e}"
 
 class Colors:
     def __init__(self) -> None:
@@ -77,35 +80,79 @@ class App(Tk):
         super().__init__()
         self.downloader = downloader
         self.video_info = None
+
+        self.icon = Path("image/file.png")
+
         # Create the window for the GUI
         self.title("Video Downloader")
         self.geometry("800x500")
         self.url_input = StringVar()
         self.widget(Colors())
 
-    def show_cli(self, title) -> None:
+    def show_cli(self, title = None, info: str = None) -> None:
         self.cli_command.config(state="normal")
-        cli_output = f"Downloading: {title}\n"
+        cli_output = ""  # Initialize cli_output here
+
+        if info:
+            cli_output += f"{info}\n"
+
+        if title:
+            cli_output += f"Downloading: {title}\n"
+        
         self.cli_command.insert(END, cli_output)
         self.cli_command.see(END)
         self.cli_command.config(state="disabled")
 
     def onClick(self) -> None:
         try:
-            print("Button Click")
+            # print("Button Click")
             url = self.url_input.get()
             video_info = self.downloader.get_video_info(url)
-            if video_info:
-                self.show_cli(video_info.title)
-                print(f"Title: {video_info.title}")
-                self.downloader.download_video(video_info)
+            if isinstance(video_info, str):  # Check if it's an error message
+                self.show_cli(info=video_info)
+                return
+            
+            self.show_cli(video_info.title)
+            download_message = self.downloader.download_video(video_info)
+            self.show_cli(info=download_message)
+
         except Exception as e:
             print(f"onClick function error: {e}")
+            self.show_cli(info=f"onClick function error {e}")
             # print(f"Duration: {video_info.duration} seconds")
+
+    def browseFiles(self):
+        try:
+            filename = filedialog.askopenfilename(initialdir="/", title="Select a File", filetypes=[("Text files", "*.txt")])
+            self.show_cli(info=f"Open file: {filename}")
+            with open(filename, "r") as file:
+                urls = file.readlines()
+
+            for url in urls:
+                url = url.strip()
+                if not url:
+                    continue
+
+                video_info = self.downloader.get_video_info(url)
+                if isinstance(video_info, str):
+                    self.show_cli(info=video_info)
+                    continue
+                
+                self.show_cli(video_info.title)
+                try:
+                    download_message = self.downloader.download_video(video_info)
+                    self.show_cli(info=download_message)
+                except Exception as download_error:
+                    self.show_cli(info=f"Download failder for {video_info.title}: {download_error}")
+                
+        except Exception as e:
+            print(f"Error: {e}")
+            return f"Error: {e}"
     
     def widget(self, color: Colors):
         main_f = Frame(self)
         main_f.pack(padx=34, pady=34)
+
 
         # Create the frame for the title
         title_f = Frame(main_f, height=30, width=210)
@@ -127,9 +174,16 @@ class App(Tk):
         url_entry = Entry(function_f, font=("Arial", 12), textvariable=self.url_input)
         url_entry.place(x=0, y=0, width=387, height=40)
 
+        # select file button
+        image_obj = PhotoImage(file=self.icon)
+        file_button = Button(function_f, image=image_obj, command=self.browseFiles, width=40)
+        file_button.image = image_obj
+        file_button.place(x=394, y=0, width=40, height=40)
+        # file_button.pack()
+
         # download button
         download_button = Button(function_f, text="Download", command=self.onClick)
-        download_button.place(x=395, y=0, width=170, height=40)
+        download_button.place(x=441, y=0, width=124, height=40)
 
         # CLI frame
         self.cli_f = Frame(content_f, height=223, width=565, bg=color.get_color("light"))
@@ -147,13 +201,6 @@ def main():
     app = App(downloader)
     try:
         app.mainloop()
-        # url = input("Enter the video URL: ")
-        # video_info = downloader.get_video_info(url)
-        
-        # if video_info:
-        #     print(f"Title: {video_info.title}")
-        #     print(f"Duration: {video_info.duration} seconds")
-        #     downloader.download_video(video_info)
             
     except KeyboardInterrupt:
         print("\nOperation cancelled by user")
